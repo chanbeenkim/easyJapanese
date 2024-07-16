7dcfc39637ac49d9fac05963d5987eef63cfcca9
beenfilm94@gmail.com <kcb9582@naver.com>
2024-07-16 17:43:31 +0900

    view: 오디오 카드 구현

---
+++ lib/pages/widgets/japanese_card_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+class JapaneseCardWidget extends StatelessWidget {
+  final String japaneseAudio;
+  final String title;
+  final AudioPlayer audioPlayer;
+  const JapaneseCardWidget(
+      {super.key,
+      required this.japaneseAudio,
+      required this.title,
+      required this.audioPlayer});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () async {
+        await audioPlayer.stop();
+        await audioPlayer.play(
+          AssetSource(
+            japaneseAudio,
+          ),
+        );
+      },
+      child: Ink(
+        height: 80,
+        decoration: BoxDecoration(
+          color: Colors.black54,
+          borderRadius: BorderRadius.circular(
+            8,
+          ),
+        ),
+        child: Center(
+          child: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
