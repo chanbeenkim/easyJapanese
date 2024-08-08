e4a1578aa755a87e312c0f594a82ca7006593ea3
beenfilm94@gmail.com <kcb9582@naver.com>
2024-08-08 23:13:30 +0900

    ✨feat: 추천 콘텐츠 그리드뷰로 구현 및 파이어스토어 price 필드 추가

--- lib/view/recommend_category/common_convenience_page.dart
+++ lib/view/recommend_category/common_convenience_page.dart
@@ -11,24 +11,57 @@ class CommonConveniencePage extends ConsumerWidget {
         ref.watch(commonConvenienceServiceProvider);
 
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('Convenience Store Menu'),
+        backgroundColor: Colors.white,
+        title: const Text(
+          '공통',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
       ),
       body: convenienceStoreAsyncValue.when(
         data: (docs) {
-          return ListView(
-            children: docs.map((document) {
-              return ListTile(
-                leading: Image.network(
-                  document['imageUrl'],
+          return GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, // 한 행에 2개의 항목을 표시
+              childAspectRatio: 0.75, // 항목의 가로 세로 비율 조정
+            ),
+            itemCount: docs.length,
+            itemBuilder: (context, index) {
+              final document = docs[index];
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.network(
+                      document['imageUrl'],
+                      width: double.infinity,
+                      height: MediaQuery.of(context).size.height * 0.2,
+                      fit: BoxFit.cover,
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      document['title'],
+                      style: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      document['subTitle'],
+                      style: const TextStyle(fontSize: 12),
+                    ),
+                    Text(
+                      document['price'],
+                      style: const TextStyle(fontSize: 12),
+                    ),
+                  ],
                 ),
-                title: Text(
-                  document['title'],
-                  style: const TextStyle(fontSize: 40),
-                ),
-                subtitle: Text(document['subTitle']),
               );
-            }).toList(),
+            },
           );
         },
         loading: () => const Center(child: CircularProgressIndicator()),
