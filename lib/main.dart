3d11f8ef318bae4550f858685febced7679e8376
beenfilm94@gmail.com <kcb9582@naver.com>
2024-08-08 16:49:53 +0900

    ⚙️chore: 파이어베이스 초기 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:easy/firebase_options.dart';
 import 'package:easy/view/page/main_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -6,6 +8,10 @@ import 'package:intl/date_symbol_data_local.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
